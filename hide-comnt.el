8aeb14699a1e9fc495ddce3d9771cd7a33e088af
Sam Bonnekamp <sam@bonnekamp.net>
2025-09-25 20:48:29 +0800

    removed old code

--- hide-comnt.el
+++ hide-comnt.el
@@ -54,49 +54,6 @@
 (global-set-key (kbd "C-x r") 'process-marked-lines)
 
 
-;;;###autoload
-(defun hide/show-comments (&optional hide/show start end)
-  "Hide or show comments from START to END.
-Interactively, hide comments, or show them if you use a prefix arg.
-Interactively, START and END default to the region limits, if active.
-Otherwise, including non-interactively, they default to `point-min'
-and `point-max'.
-
-Uses `save-excursion', restoring point.
-
-Be aware that using this command to show invisible text shows *all*
-such text, regardless of how it was hidden.  IOW, it does not just
-show invisible text that you previously hid using this command.
-
-From Lisp, a HIDE/SHOW value of `hide' hides comments.  Other values
-show them.
-
-This function does nothing in Emacs versions prior to Emacs 21,
-because it needs `comment-search-forward'."
-  (interactive
-   (cons (if current-prefix-arg 'show 'hide)
-         (if (or (not mark-active) (null (mark)) (= (point) (mark)))
-             (list (point-min) (point-max))
-           (if (< (point) (mark)) (list (point) (mark)) (list (mark) (point))))))
-  (when (require 'newcomment nil t)     ; `comment-search-forward', Emacs 21+.
-    (unless start (setq start  (point-min)))
-    (unless end   (setq end    (point-max)))
-    (unless (<= start end) (setq start  (prog1 end (setq end  start))))
-    (let ((bufmodp           (buffer-modified-p))
-          (buffer-read-only  nil)
-          cbeg cend)
-      (unwind-protect
-           (save-excursion
-             (goto-char start)
-             (while (and (< start end) (setq cbeg  (comment-search-forward end 'NOERROR)))
-               (setq cend  (if (string= "" comment-end)
-                               (min (1+ (line-end-position)) (point-max))
-                             (search-forward comment-end end 'NOERROR)))
-               (when (and cbeg cend)
-                 (if (eq 'hide hide/show)
-                     (put-text-property cbeg cend 'invisible t)
-                   (put-text-property cbeg cend 'invisible nil)))))
-        (set-buffer-modified-p bufmodp)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
